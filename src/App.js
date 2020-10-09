1622b88614852c40637d83812d2ef87e81f76dcc
FlorianPx <florian.poux@hotmail.fr>
2020-10-09 13:40:40 +0200

    Challenge 2

--- src/App.js
+++ src/App.js
@@ -5,7 +5,24 @@ import Contact from "./components/Contact";
 function App() {
   return (
     <div className="App">
-      <Contact />
+      <Contact
+        picture="https://randomuser.me/api/portraits/men/39.jpg"
+        firstName="Murray"
+        name="Paul"
+        online={true}
+      />
+      <Contact
+        picture="https://randomuser.me/api/portraits/men/3.jpg"
+        firstName="Mcdona"
+        name="Loren"
+        online={false}
+      />
+      <Contact
+        picture="https://randomuser.me/api/portraits/men/11.jpg"
+        firstName="Alvarez"
+        name="Luis"
+        online={true}
+      />
     </div>
   );
 }
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,20 +1,17 @@
 import React from "react";
 import "./Contact.css";
 
-function Contact() {
-  const picture = "https://randomuser.me/api/portraits/men/39.jpg";
-  const firstName = "Murray";
-  const name = "Paul";
-  const online = true;
-
+function Contact(props) {
   return (
     <div className="Contact">
-      <img className="avatar" src={picture} alt="Picture of Paul Murray" />
+      <img className="avatar" src={props.picture} />
       <div className="text">
-        <h1 className="name">{`${firstName} ${name}`}</h1>
+        <h1 className="name">{`${props.firstName} ${props.name}`}</h1>
         <div className="status">
-          <span className={online ? "status-online" : "status-offline"}></span>
-          <p className="status-text">{online ? "Online" : "Offline"}</p>
+          <span
+            className={props.online ? "status-online" : "status-offline"}
+          ></span>
+          <p className="status-text">{props.online ? "Online" : "Offline"}</p>
         </div>
       </div>
     </div>
